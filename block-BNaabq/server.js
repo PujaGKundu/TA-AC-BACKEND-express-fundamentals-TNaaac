032fb05dd4833fb6bd99ef39e91dde2ed96b3485
PujaGKundu <pujakundu100@gmail.com>
2022-01-31 21:47:11 +0530

    Third party Middleware

--- block-BNaabq/server.js
+++ block-BNaabq/server.js
@@ -4,27 +4,22 @@ var cookieP = require("cookie-parser");
 
 var app = express();
 
-//Morgan
-app.use(logger("dev"));
-
-//Cookie-parser
+app.use(logger("tiny"));
 app.use(cookieP());
-
 app.use(express.static(__dirname + "/public"));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ exteded: false }));
 
-app.use("/about", (req, res, next) => {
-  res.cookie("username", "Puja");
+app.use((req, res, next) => {
+  console.log(req.cookies);
   next();
 });
 
-app.get("/", (req, res) => {
-  req.cookies;
+app.use("/about", (req, res, next) => {
+  res.cookie("username", "Puja");
+  res.end("About Page");
 });
 
-app.listen(3000, () => {
+app.app.listen(3000, () => {
   console.log("Server listening to port 3k");
 });
